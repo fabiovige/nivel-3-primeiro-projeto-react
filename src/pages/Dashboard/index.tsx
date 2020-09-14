cdf5e904559a796dbf426c5fb59d900e71d155c4
Fabio Vige <fabiovige@gmail.com>
2020-09-14 15:47:51 -0300

    estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars2.githubusercontent.com/u/670083?s=460&u=46e1797841451f5aee6fed85a1b66bb2f6920362&v=4" alt="Fabio Vige"/>
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Simplicidade e funcionalidades caminham juntos</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars2.githubusercontent.com/u/670083?s=460&u=46e1797841451f5aee6fed85a1b66bb2f6920362&v=4" alt="Fabio Vige"/>
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Simplicidade e funcionalidades caminham juntos</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars2.githubusercontent.com/u/670083?s=460&u=46e1797841451f5aee6fed85a1b66bb2f6920362&v=4" alt="Fabio Vige"/>
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Simplicidade e funcionalidades caminham juntos</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+      </Repositories>
+
+    </>
+  )
 };
 
 export default Dashboard;
